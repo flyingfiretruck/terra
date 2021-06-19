2eaa5a62c6d9f7825237f94b24f84a3508e5e9d8
Helen <firetruk@umich.edu>
2021-06-20 08:51:12 +0900

    advanced email

--- client/components/EmailList.jsx
+++ client/components/EmailList.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 const EmailList= () => (
   <div className="email_list">
+    <div className="email_text">
+      <div className="email_text_head_1">Get on our list</div>
+      <div className="email_text_head_2">We promise it's worth it.</div>
+    </div>
     <img className="email_background" src="assets/picture_1.jpg"/>
   </div>
 );
