f3de3f9c55540706148d22419526aea557e3746a
Helen <firetruk@umich.edu>
2021-06-24 09:49:01 +0900

    deleted footer for now

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Landing from './components/Landing.jsx'
-import Footer from './widgets/Footer.jsx'
 import Navigation from './widgets/Navbar.jsx'
 import EmailList from './components/EmailList.jsx'
 
