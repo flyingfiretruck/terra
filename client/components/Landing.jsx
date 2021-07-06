add169edf781a5dc14874acaae7577789d449875
Helen <firetruk@umich.edu>
2021-07-07 08:09:43 +0900

    changes made to fix bug, hooks for animation

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const Landing = () => (
-  <div className="landing">
-    <div className="intro">
-      <div className="intro-words-1 animate__animated animate__slideInRight">
-        <div className="intro-1">Spatial</div>
-        <div className="intro-2">development</div>
-        <div className="intro-3">for a</div>
-        <div className="intro-4">better</div>
-        <div className="intro-5">future.</div>
+function Landing() {
+  const [animate] = useState(false);
+  // useEffect (() => {
+  //   animate = true;
+  // })
+  return (
+    <div className="landing">
+      <div className="intro">
+        <div className={animate ?
+        'intro-words-1-visible animate__animated animate__fadeInUp' :
+        'intro-words-1-invisible'}
+        >
+          <div className="intro-1">Spatial</div>
+          <div className="intro-2">development</div>
+          <div className="intro-3">for a</div>
+          <div className="intro-4">better</div>
+          <div className="intro-5">future.</div>
+        </div>
+        <video className="intro-video-1" loop={true} autoPlay="autoPlay" muted>
+          <source src="assets/video_1.mp4" type="video/mp4"/>
+        </video>
       </div>
-      <video className="intro-video-1" loop={true} autoPlay="autoPlay" muted>
-        <source src="assets/video_1.mp4" type="video/mp4"/>
-      </video>
+      <span className="intro-words-2">
+        Based in Korea, we provide technical consultation globally in areas of
+        spatial planning, land development and real estate investment at national,
+        regional and local level. We are happy to serve you the best practices
+        around the world.
+      </span>
     </div>
-    <span className="intro-words-2">
-      Based in Korea, we provide technical consultation globally in areas of
-      spatial planning, land development and real estate investment at national,
-      regional and local level. We are happy to serve you the best practices
-      around the world.
-    </span>
-
-  </div>
-);
+  )
+}
 
 export default Landing;
--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const path = require('path');
 
 const port = 3000;
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 app.use(express.static(path.join(__dirname, '..')));
+
 
 app.use('/', (req, res) => {
   res.render('index.html')
